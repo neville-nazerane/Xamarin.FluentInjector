87abd5c664ddc91d56b1e61ff56dcbc5aa51dde1
Neville <live.neville@live.com>
2020-05-10 18:17:07 -0700

    made functions virtual for base viewmodel

--- src/Xamarin.FluentInjector/InjectorViewModelBase.cs
+++ src/Xamarin.FluentInjector/InjectorViewModelBase.cs
@@ -9,13 +9,12 @@ using Xamarin.Forms;
 namespace Xamarin.FluentInjector
 {
 
-    [Obsolete("Use the IXamAccess interface I have not yet provided")]
-    public class InjectorViewModelBase : INotifyPropertyChanged // : BindableObject
+    public class InjectorViewModelBase : INotifyPropertyChanged
     {
 
         internal Page CurrentPage { get; set; }
 
-        public bool IsBusy { get => CurrentPage.IsBusy; set => CurrentPage.IsBusy = value; }
+        public virtual bool IsBusy { get => CurrentPage.IsBusy; set => CurrentPage.IsBusy = value; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -23,17 +22,17 @@ namespace Xamarin.FluentInjector
 
         public virtual async Task<Page> NavigateAsync<TViewModel>() => await InjectionControl.NavigateAsync<TViewModel>();
 
-        public async Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons) 
+        public virtual async Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons) 
             => await CurrentPage.DisplayActionSheet(title, cancel, destruction, buttons);
 
-        public async Task DisplayAlert(string title, string message, string cancel) => await CurrentPage.DisplayAlert(title, message, cancel);
+        public virtual async Task DisplayAlert(string title, string message, string cancel) => await CurrentPage.DisplayAlert(title, message, cancel);
 
-        public async Task<bool> DisplayAlert(string title, string message, string accept, string cancel) => await CurrentPage.DisplayAlert(title, message, accept, cancel);
+        public virtual async Task<bool> DisplayAlert(string title, string message, string accept, string cancel) => await CurrentPage.DisplayAlert(title, message, accept, cancel);
 
-        public async Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = null) 
+        public virtual async Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = null) 
             => await CurrentPage.DisplayPromptAsync(title, message, accept, cancel, placeholder, maxLength, keyboard, initialValue);
 
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(storage, value))
                 return false;
@@ -43,7 +42,7 @@ namespace Xamarin.FluentInjector
             return true;
         }
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
