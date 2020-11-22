147d0b240443f6c891380091fe4a8327d0a0a742
Neville <live.neville@live.com>
2020-11-22 05:05:22 -0700

    more control to page control

--- src/Xamarin.FluentInjector/Internals/PageControl.cs
+++ src/Xamarin.FluentInjector/Internals/PageControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.FluentInjector.Configs;
 using Xamarin.FluentInjector.Utilities;
 using Xamarin.Forms;
 
@@ -11,8 +12,14 @@ namespace Xamarin.FluentInjector.Internals
     {
 
         internal Page _page;
+        private readonly InjectionConfiguration _configuration;
 
         public object Page => _page;
+
+        public PageControl(InjectionConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons)
             => _page.DisplayActionSheet(title, cancel, destruction, buttons);
@@ -26,6 +33,27 @@ namespace Xamarin.FluentInjector.Internals
         public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = "")
             => _page.DisplayPromptAsync(title, message, accept, cancel, placeholder, maxLength, keyboard, initialValue);
 
+
+
+        public Page ResolvePage<T>(Action<T> addData = null) => _configuration.ResolvePage(addData);
+
+        public async Task<Page> NavigateAsync<T>(Action<T> addData = null)
+        {
+            var page = _configuration.ResolvePage(addData);
+            await _configuration.NavigateAsync(_configuration.App, page);
+            return page;
+        }
+
+        public Task PopAsync() => _page.Navigation.PopAsync();
+        public Task PopModalAsync() => _page.Navigation.PopModalAsync();
+        public Task PopToRootAsync() => _page.Navigation.PopToRootAsync();
+
+        public async Task<Page> PushModalAsync<T>(Action<T> addData = null)
+        {
+            var page = ResolvePage(addData);
+            await _page.Navigation.PushModalAsync(page);
+            return page;
+        }
 
     }
 }
--- src/Xamarin.FluentInjector/Utilities/IPageControl.cs
+++ src/Xamarin.FluentInjector/Utilities/IPageControl.cs
@@ -8,14 +8,24 @@ namespace Xamarin.FluentInjector.Utilities
 {
     public interface IPageControl
     {
-
         object Page { get; }
 
         Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons);
         Task DisplayAlert(string title, string message, string cancel);
         Task<bool> DisplayAlert(string title, string message, string accept, string cancel);
-        Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = "");
-
-
+        Task<string> DisplayPromptAsync(string title,
+                                        string message,
+                                        string accept = "OK",
+                                        string cancel = "Cancel",
+                                        string placeholder = null,
+                                        int maxLength = -1,
+                                        Keyboard keyboard = null,
+                                        string initialValue = "");
+        Task<Page> NavigateAsync<T>(Action<T> addData = null);
+        Task PopAsync();
+        Task PopModalAsync();
+        Task PopToRootAsync();
+        Task<Page> PushModalAsync<T>(Action<T> addData = null);
+        Page ResolvePage<T>(Action<T> addData = null);
     }
 }
